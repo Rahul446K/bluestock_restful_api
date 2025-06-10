1ebbdb18e90385863bc10d838fc001cad383228c
Rahul Kumar <rahulchaudhary4542075@gmail.com>
2025-06-10 19:07:13 +0530

    Normalize all line endings

---
+++ backend/ipo_project/ipo_api/serializers.py
@@ -0,0 +1,43 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from .models import Company, IPO, Document
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, style={
+                                     'input_type': 'password'})
+    email = serializers.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        return user
+
+
+class CompanySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Company
+        fields = '__all__'
+
+
+class IPOSerializer(serializers.ModelSerializer):
+    company_name = serializers.CharField(
+        source='company.company_name', read_only=True)
+
+    class Meta:
+        model = IPO
+        fields = '__all__'
+
+
+class DocumentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Document
+        fields = '__all__'
