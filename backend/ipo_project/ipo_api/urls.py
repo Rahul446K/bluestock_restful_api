99943a4bc7fd3a41d48d7581b2bf2bb17eaa7fab
Gyaan <gyanpradhan0912@gmail.com>
2025-06-12 22:04:47 +0530

    Registered custom login, token refresh, and logout routes in API URLs

--- backend/ipo_project/ipo_api/urls.py
+++ backend/ipo_project/ipo_api/urls.py
@@ -5,6 +5,8 @@ from .views import (
     CompanyViewSet,
     IPOViewSet,
     DocumentViewSet,
+    LoginView,
+    LogoutView
 )
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -23,4 +25,6 @@ urlpatterns = [
     path('signup/', RegisterView.as_view(), name='register'),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
