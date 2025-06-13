30e4ece745da04e8143c18f3e48f6630a40ec271
Tiruselvibolla <141702830+Tiruselvibolla@users.noreply.github.com>
2025-06-13 22:05:10 +0530

    Add files via upload

---
+++ frontend/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import IPOCreateForm from './components/IPOCreateForm';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <IPOCreateForm />
+    </div>
+  );
+}
+
+export default App;
+import IPOCreateForm from './components/IPOCreateForm';
+
+function App() {
+  return (
+    <div className="min-h-screen p-4 bg-gray-100">
+      <IPOCreateForm />
+    </div>
+  );
+}
+
